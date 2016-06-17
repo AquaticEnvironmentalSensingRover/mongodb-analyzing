013f6d8da425bf90ec1644afceec8c0963b4ef93
pgaitskell <pgaitskell@lincolnschool.org>
2016-06-17 16:49:08 -0400

    Add TimeUtil library

---
+++ lib/time_util.py
@@ -0,0 +1,16 @@
+import time
+import datetime
+
+class TimeUtil:
+    '''Static functions for converting time related functions'''
+    
+    # method for converting between epoch time to datetime format (Y-M-D H:M:S.s)
+    # epoch: number of seconds since January 0th, 1970, T00:00
+    @staticmethod
+    def EpochToDateTime(epoch):
+        return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S.%f')
+    
+    # method for converting between datetime and epoch form  
+    @staticmethod
+    def DateTimeToEpoch(dateTime):
+        return time.mktime(dateTime.timetuple()) + dateTime.microsecond * 1e-6
