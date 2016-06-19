958f3bc1054f9336cb369a78e54b643ce278d6b5
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-19 12:55:59 -0400

    Add loop to temp data analyzer

--- temp_data_analyze.py
+++ temp_data_analyze.py
@@ -15,30 +15,33 @@ mongo = MongoClient(host="10.0.2.81")
 
 dbCol = (mongo[db])[col]
 
-data = []
-
-for ii in range(sensorAmount):
-    newData = []
-    for jj in dbCol.find({"atype":"TEMP", "itype":ii}):
-        newData.append([ jj["ts"], jj["param"] ])
+while True:
+    data = []
+    
+    for ii in range(sensorAmount):
+        newData = []
+        for jj in dbCol.find({"atype":"TEMP", "itype":ii}):
+            newData.append([ jj["ts"], jj["param"] ])
+            
+        data.append(newData)
         
-    data.append(newData)
+    t0 = time.time()
     
-t0 = time.time()
-
-args=[]
-for index, ii in enumerate(data):
-    x, y=zip(*ii)
-    args.append( np.add(x,-t0) )
-    args.append(y)
-    args.append(colors[index])
+    args=[]
+    for index, ii in enumerate(data):
+        x, y=zip(*ii)
+        args.append( np.add(x,-t0) )
+        args.append(y)
+        args.append(colors[index])
+        
     
-# Plot data
-plt.figure(1)
-plt.clf()
-plt.plot(*args)
-plt.title('Latest data %f' % t0 )
-plt.axis([-100, 5, 22, 26])
-plt.ylabel("Temperature (degC)")
-plt.xlabel("Time (s)")
-plt.show()
+    # Plot data
+    plt.figure(1)
+    plt.clf()
+    plt.plot(*args)
+    plt.title('Latest data %f' % t0 )
+    plt.axis([-100, 5, 22, 26])
+    plt.ylabel("Temperature (degC)")
+    plt.xlabel("Time (s)")
+    plt.show()
+    time.sleep(1)
