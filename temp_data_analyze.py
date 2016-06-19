bfd0ca71f900ef71bc4a1b3e4c1e3e51b411421c
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-19 13:07:20 -0400

    Revert "Add loop to temp data analyzer"
    This reverts commit 9852020da0640eb5d0577d101fd8c93c4528bdf2.

--- temp_data_analyze.py
+++ temp_data_analyze.py
@@ -15,33 +15,30 @@ mongo = MongoClient(host="10.0.2.81")
 
 dbCol = (mongo[db])[col]
 
-while True:
-    data = []
-    
-    for ii in range(sensorAmount):
-        newData = []
-        for jj in dbCol.find({"atype":"TEMP", "itype":ii}):
-            newData.append([ jj["ts"], jj["param"] ])
-            
-        data.append(newData)
+data = []
+
+for ii in range(sensorAmount):
+    newData = []
+    for jj in dbCol.find({"atype":"TEMP", "itype":ii}):
+        newData.append([ jj["ts"], jj["param"] ])
         
-    t0 = time.time()
+    data.append(newData)
     
-    args=[]
-    for index, ii in enumerate(data):
-        x, y=zip(*ii)
-        args.append( np.add(x,-t0) )
-        args.append(y)
-        args.append(colors[index])
-        
+t0 = time.time()
+
+args=[]
+for index, ii in enumerate(data):
+    x, y=zip(*ii)
+    args.append( np.add(x,-t0) )
+    args.append(y)
+    args.append(colors[index])
     
-    # Plot data
-    plt.figure(1)
-    plt.clf()
-    plt.plot(*args)
-    plt.title('Latest data %f' % t0 )
-    plt.axis([-100, 5, 22, 26])
-    plt.ylabel("Temperature (degC)")
-    plt.xlabel("Time (s)")
-    plt.show()
-    time.sleep(1)
+# Plot data
+plt.figure(1)
+plt.clf()
+plt.plot(*args)
+plt.title('Latest data %f' % t0 )
+plt.axis([-100, 5, 22, 26])
+plt.ylabel("Temperature (degC)")
+plt.xlabel("Time (s)")
+plt.show()
