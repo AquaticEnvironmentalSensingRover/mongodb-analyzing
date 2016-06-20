d46212431cf8b6db898f71553d9f01ccc2b5bce2
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-20 14:05:24 -0400

    Updating temp data analyzer for 5 sensors

--- temp_data_analyze.py
+++ temp_data_analyze.py
@@ -4,12 +4,12 @@ import matplotlib.pyplot as plt
 from pymongo import MongoClient
 import time
 
-colors = ["r","g","b","c"]
+colors = ["r","g","b","c","k"]
 
-db = "test_data1"
+db = "test_data2"
 col = "tempData"
 
-sensorAmount = 4
+sensorAmount = 5
 
 mongo = MongoClient(host="10.0.2.81")
 
@@ -26,19 +26,17 @@ for ii in range(sensorAmount):
     
 t0 = time.time()
 
-args=[]
-for index, ii in enumerate(data):
-    x, y=zip(*ii)
-    args.append( np.add(x,-t0) )
-    args.append(y)
-    args.append(colors[index])
-    
 # Plot data
 plt.figure(1)
 plt.clf()
-plt.plot(*args)
 plt.title('Latest data %f' % t0 )
 plt.axis([-100, 5, 22, 26])
 plt.ylabel("Temperature (degC)")
 plt.xlabel("Time (s)")
+
+for index, ii in enumerate(data):
+    x, y=zip(*ii)
+    plt.plot(np.add(x,-t0), y, colors[index], label=("Temp: " + str(index)))
+    
+plt.legend()
 plt.show()
