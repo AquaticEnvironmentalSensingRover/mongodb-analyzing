a2916738ff7e79e5984ab49af6c9effc70af2499
pgaitskell <pgaitskell@lincolnschool.org>
2016-06-21 18:50:31 -0400

    Removed data array, edited param key

--- pressure_data_analyze.py
+++ pressure_data_analyze.py
@@ -11,28 +11,25 @@ col = raw_input("Col Name: ")
 
 dbCol = (mongo[db])[col]
 
-data = []
 dataPressr = []
 dataTime = []
 
 t0 = time.time()
 
-for ii in db.find({"atype":"PRESR"}):
-    dataPressr.append( (ii["param"])["mbar"])
+for ii in dbCol.find({"atype":"PRESR"}):
+    dataPressr.append( (ii["param"])["pressure"])
     dataTime.append( ii["ts"] - t0)
-
-data.append(dataPressr, dataTime)
     
 t0 = time.time()
 
 # Plot data
 plt.figure(1)
 plt.clf()
+plt.autoscale(True)
 plt.title('Latest data %f: Pressure vs. Time' % t0 )
-plt.axis([-100, 5, 22, 26])
 plt.ylabel("Pressure (mbar)")
 plt.xlabel("Time (s)")
 
-plt.plot(data, "ro")
+plt.plot(dataTime, dataPressr, "ro")
 
 plt.show()
