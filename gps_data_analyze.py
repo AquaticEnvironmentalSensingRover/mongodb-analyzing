a60040897ff8e045c82bfe4511ce673e30dcee42
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-06-21 16:18:58 -0400

    Updating world map and adding house picture in GPS data analyzer

--- gps_data_analyze.py
+++ gps_data_analyze.py
@@ -26,11 +26,25 @@ plt.xlabel("Longitude (deg)")
 plt.ylabel("Latitude (deg)")
 
 img = imread("maps/map.jpg")
-plt.imshow(img, zorder=0, extent=[-180, 180, 90, -90])
+plt.imshow(img, zorder=0, extent=[-180, 180, -90, 90])
+
+img = imread("maps/house.jpg")
+plt.imshow(img, zorder=0, extent=[-71.343431, -71.342690, 41.739744, 41.740059])
+
+#Center of the house should be located at   X -71.343310 , Y 41.739910,
+
+
+#UL: 41.740019, -71.343472
+#LL: 41.739744, -71.343431
+#LR: 41.739802, -71.342680
+#UR: 41.740059, -71.342690
+
+# Plot the center of the house
+plt.plot( -71.343310 , 41.739910 , 'rd' ) 
 
 x, y=zip(*data)
 plt.plot(x, y, "bo")
-ax = plt.gca()
-ax.invert_yaxis()
+'''ax = plt.gca()
+ax.invert_yaxis()'''
 
 plt.show()
