479da3182bf95bdd06a2dd621106098b28d03560
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-07-02 19:32:38 -0400

    Add server address selector to analyze util

--- lib/analyze_util.py
+++ lib/analyze_util.py
@@ -1,3 +1,14 @@
+DEFAULTS = {"server":["localhost", "10.0.2.197", "192.168.42.50"]}
+
+def serverAddressSelector():
+    print("\nUse index number of IP address\nDefault server IP Addresses:\n" + str(DEFAULTS["server"]))
+    ipAddress = raw_input("Server IP Address: ")
+    
+    try:
+        return (DEFAULTS["server"])[int(ipAddress)]
+    except ValueError:
+        return ipAddress
+
 def dbColSelector(mongo):
     print("Databases:\n" + str(sorted(mongo.database_names())))
     db = raw_input("DB Name: ")
