017bf9c5695b703aa042fdc228199fb386946063
Lucien Gaitskell <luciengaitskell@users.noreply.github.com>
2016-07-02 19:32:57 -0400

    Add newline to print in analyze util

--- lib/analyze_util.py
+++ lib/analyze_util.py
@@ -10,7 +10,7 @@ def serverAddressSelector():
         return ipAddress
 
 def dbColSelector(mongo):
-    print("Databases:\n" + str(sorted(mongo.database_names())))
+    print("\nDatabases:\n" + str(sorted(mongo.database_names())))
     db = raw_input("DB Name: ")
     print("\nCollections:\n" + str(sorted(mongo[db].collection_names())))
     col = raw_input("Col Name: ")
