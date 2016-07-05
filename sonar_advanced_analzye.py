3a8f2d5a0ce2f4a6967c8fdebfc49955f17eba7a
pgaitskell <pgaitskell@lincolnschool.org>
2016-07-05 12:47:04 -0400

    Plots all sonar data (manual and database) and uses the sonar_manual_data class to plot hand sonar data

--- sonar_advanced_analzye.py
+++ sonar_advanced_analzye.py
@@ -5,28 +5,40 @@ import time
 from lib.time_util import TimeUtil
 from matplotlib.dates import DateFormatter
 
+from sonar_manual_data import *        
+
 mongo = MongoClient(host="localhost")
 formatter = DateFormatter('%H:%M:%S')
 
-print("Databases:\n" + str(mongo.database_names()))
-db = raw_input("DB Name: ")
-print("\nCollections:\n" + str(mongo[db].collection_names()))
-col = raw_input("Col Name: ")
+#print("Databases:\n" + str(mongo.database_names()))
+# db = raw_input("DB Name: ")
+db = 'AESR_20160629T185315'
+#print("\nCollections:\n" + str(mongo[db].collection_names()))
+#col = raw_input("Col Name: ")
+col = 'data'
 
 dbCol = (mongo[db])[col]
 
-t0 = time.time()
+# t0 = time.time()
 
+scaleAirCmToWaterM = 1./100.*4.3
 dataDepth = []
 dataTime = []
 randEpoch = None
 
 for index, ii in enumerate(dbCol.find({"atype":"SONAR"})):
-    dataDepth.append((ii["param"])/100*4.3)
-    dataTime.append(dates.datestr2num(TimeUtil.EpochToTime(ii["ts"])))
     
-    if index == 0:
-        randEpoch = TimeUtil.EpochToDate(ii["ts"])
+    if ii["param"] < 750: # Values >= 750 are bad read 
+        dataDepth.append((ii["param"]) * scaleAirCmToWaterM )
+        dataTime.append(TimeUtil.EpochToTime(ii["ts"]))
+        
+        if index == 0:
+            randEpoch = TimeUtil.EpochToDate(ii["ts"])
+            
+ 
+mdataStandardDate = np.array( [ TimeUtil.EpochToTime(tt) for tt in mdataDateEpoch ] )    
+                
+            
 
 # Plot data
 plt.figure(1)
@@ -38,6 +50,10 @@ plt.xlabel("Time")
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)  
 plt.xticks(rotation=45)
 
+mtop=25; mbot = 0
 plt.plot_date(dataTime, dataDepth, "ko")
+plt.plot_date(mdataStandardDate, mdataDepth, 'ro')
+for x in mdataStandardDate :
+    plt.plot_date([x , x], [mbot,mtop],'r--' ) 
 
 plt.show()
