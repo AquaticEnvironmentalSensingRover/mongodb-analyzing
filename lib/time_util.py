d6a5f95d4f614124d4ec22b59c79c42b66585762
pgaitskell <pgaitskell@lincolnschool.org>
2016-07-17 22:59:07 -0400

    Reverted to library rather than class

--- lib/time_util.py
+++ lib/time_util.py
@@ -1,29 +1,29 @@
 import time
 import datetime
 
-class TimeUtil:
-    '''Static functions for converting time related functions'''
+# class TimeUtil:
+'''Static functions for converting time related functions'''
     
     #method for converting between epoch time to datetime format (Y-M-D H:M:S.s)
     # epoch: number of seconds since January 0th, 1970, T00:00
-    @staticmethod
-    def EpochToDateTime(epoch):
-        return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S.%f')
+#    @staticmethod
+def EpochToDateTime(epoch):
+    return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d %H:%M:%S.%f')
     
     # method for converting between epoch time to a standard time format (H:M:S.s)
     # use only if data is mentioned in analysis method
-    @staticmethod    
-    def EpochToTime(epoch):
-        return datetime.datetime.fromtimestamp(epoch)
+#    @staticmethod    
+def EpochToTime(epoch):
+    return datetime.datetime.fromtimestamp(epoch)
         
     # method for converting between epoch time to a standard date format (Y-M-D)  
-    @staticmethod
-    def EpochToDate(epoch):
-        return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
+#    @staticmethod
+def EpochToDate(epoch):
+    return datetime.datetime.fromtimestamp(epoch).strftime('%Y-%m-%d')
     
     # method for converting between datetime and epoch form  
-    @staticmethod
-    def DateTimeToEpoch(dateTime):
-        return time.mktime(dateTime.timetuple()) + dateTime.microsecond * 1e-6
+#    @staticmethod
+def DateTimeToEpoch(dateTime):
+    return time.mktime(dateTime.timetuple()) + dateTime.microsecond * 1e-6
 
-print  TimeUtil.EpochToTime(time.time())
+#print  TimeUtil.EpochToTime(time.time())
