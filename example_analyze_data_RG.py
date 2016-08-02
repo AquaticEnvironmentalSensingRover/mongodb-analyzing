b27f539c397454f73df957f9f392395105bd67fa
gaitskell <Richard_Gaitskell@brown.edu>
2016-08-02 06:09:52 -0400

    Example analysis code
    Example analysis code

--- example_analyze_data_RG.py
+++ example_analyze_data_RG.py
@@ -16,44 +16,46 @@ import time
 import datetime
 from matplotlib.dates import DateFormatter
 
-
-# Cut the data for only that when on the lake
-# Epoch times for start and stop
-tStartRun004 = time.mktime( datetime.datetime(2016, 7, 16, 18, 40).timetuple() )
-tStopRun004 = time.mktime( datetime.datetime(2016, 7, 16, 19, 30).timetuple() )
-
-
-
-# Used to format the date/time axis
-formatter = DateFormatter('%H:%M:%S')
-
 # PICK SERVER OR USE PRESET
 #mongo = MongoClient(host=au.serverAddressSelector())
 mongo = MongoClient( ) # local
 #mongo = MongoClient(host="10.0.2.197")
 #mongo = MongoClient(host="172.16.0.2")
-
 # PICK COLLECTION
 #dbCol = au.dbColSelector(mongo)
-dbCol = ( mongo['AESR_20160716T184018'] )['data']
 
-# COMBINED FIGURE FROM  "all_data_analyze.py"
-plt.figure(1)
-plt.clf()
+
+# Cut the data for only that when on the lake
+# Epoch times for start and stop
+
+# Run004
+runNum = 5.2
+
+if(runNum==4) :
+    tStartRun = time.mktime( datetime.datetime(2016, 7, 16, 18, 40).timetuple() )
+    tStopRun = time.mktime( datetime.datetime(2016, 7, 16, 19, 30).timetuple() )
+    dbCol = ( mongo['AESR_20160716T184018'] )['data']
+elif( (runNum>=5) & (runNum<6) ) :
+    tStartRun = time.mktime( datetime.datetime(2016, 7, 17, 0, 0 ).timetuple() )
+    tStopRun = time.mktime( datetime.datetime(2016, 7, 17, 23, 59 ).timetuple() )
+    if runNum==5.1: dbCol = ( mongo['AESR_20160717T154442'] )['data'] 
+    if runNum==5.2: dbCol = ( mongo['AESR_20160717T165349'] )['data'] 
+    if runNum==5.3: dbCol = ( mongo['AESR_20160717T193309'] )['data'] 
+    
+
+
+# Used to format the date/time axis
+formatter = DateFormatter('%H:%M:%S')
+
+
 
 # ======================GPS======================
-plt.figure(1)
-plt.subplot(221)
-t0 = time.time()
-plt.title("GPS (Latest data: %f)" % t0 )
-plt.xlabel("Longitude (deg)")
-plt.ylabel("Latitude (deg)")
+plt.figure(100)
+plt.clf()
+#plt.subplot(221)
 
-# Enable autoscaling
-plt.autoscale(True)
 
-# Plot the center of the house
-plt.plot( -71.343310 , 41.739910 , 'rd' ) 
+#t0 = time.time()
 
 # Get GPS data
 #gpsList = [("GPS","bo"), ("IGPS","r-")]
@@ -67,14 +69,25 @@ for ii in gpsList:
         gpsLat.append((jj["param"])["lat"])
         gpsTime.append( jj["ts"] )
         
-    plt.plot(gpsLon, gpsLat , ii[1])
-    
-    # Plot GPS data
 
 # Convert the lists to Numpy ndarray for later use
 gpsLat = np.array( gpsLat )   # now convert to ndarray for later analysis
 gpsLon = np.array( gpsLon )
 gpsTime = np.array( gpsTime )
+
+                
+# Plot GPS data
+plt.title("GPS (Run: %d)" % runNum  )
+plt.xlabel("Longitude (deg)")
+plt.ylabel("Latitude (deg)")
+
+# Enable autoscaling
+plt.autoscale(True)
+
+# Plot the center of the house
+# plt.plot( -71.343310 , 41.739910 , 'rd' ) 
+        
+plt.plot(gpsLon, gpsLat , 'b.' )
 
 # Disable autoscaling to stop images from affecting scale
 plt.autoscale(False)
@@ -84,12 +97,15 @@ img = imread("maps/map.jpg")
 plt.imshow(img, zorder=0, extent=[-180, 180, -90, 90])
 
 # Add house image in specific position
-img = imread("maps/house.jpg")
-plt.imshow(img, zorder=0, extent=[-71.343431, -71.3424, 41.739744, 41.740059])
+if False:
+    img = imread("maps/house.jpg")
+    plt.imshow(img, zorder=0, extent=[-71.343431, -71.3424, 41.739744, 41.740059])
 
 # Add zoomed-out house image in specific position
-img = imread("maps/house2.jpg")
-plt.imshow(img, zorder=0, extent=[-71.34516, -71.34072, 41.738835, 41.740670])
+    img = imread("maps/house2.jpg")
+    plt.autoscale(True)
+    plt.imshow(img, zorder=0, extent=[-71.34516, -71.34072, 41.738835, 41.740670])
+    plt.autoscale(False)
 
 # UL 41.740657, -71.345162
 # LL 41.738835, -71.345169
@@ -105,25 +121,26 @@ plt.imshow(img, zorder=0, extent=[-71.330371, -71.307692, 41.730781, 41.739235])
 
 # ZOOM IN 
 
-plt.figure(1000)
-plt.clf()
-
-tStartRun004line = time.mktime( datetime.datetime(2016, 7, 16, 18, 55).timetuple() )
-tStopRun004line = time.mktime( datetime.datetime(2016, 7, 16, 19, 13).timetuple() )
-
-cut = (     ( gpsTime >= tStartRun004 )    &     (gpsTime <= tStopRun004)      ) # Make a cut
-plt.plot(gpsLon[cut], gpsLat[cut] , 'r.' )
-
-cut = (     ( gpsTime >= tStartRun004line )    &     (gpsTime <= tStopRun004line )      ) # Make a cut
-plt.plot(gpsLon[cut], gpsLat[cut] , 'y.' )
+if runNum==4 :
+    plt.figure(1000)
+    plt.clf()
+    
+    tStartRunline = time.mktime( datetime.datetime(2016, 7, 16, 18, 55).timetuple() )
+    tStopRunline = time.mktime( datetime.datetime(2016, 7, 16, 19, 13).timetuple() )
+    
+    cut = (     ( gpsTime >= tStartRun )    &     (gpsTime <= tStopRun)      ) # Make a cut
+    plt.plot(gpsLon[cut], gpsLat[cut] , 'r.' )
+    
+    cut = (     ( gpsTime >= tStartRunline )    &     (gpsTime <= tStopRunline )      ) # Make a cut
+    plt.plot(gpsLon[cut], gpsLat[cut] , 'y.' )
 
 
 # ======================PRESSURE======================
-plt.figure(1)
-plt.subplot(222)
+plt.figure(14)
+plt.clf()
 plt.autoscale(True)
 t0 = time.time()
-plt.title("Pressure vs. Time (Latest data: %f)" % t0 )
+plt.title("Pressure vs. Time (Run: %f)" % runNum )
 plt.ylabel("Pressure (mbar)")
 plt.xlabel("Time (s)")
 
@@ -146,26 +163,26 @@ plt.plot(pressrTime, pressrValue, "ro")
 
 
 # ======================SONAR======================
-plt.figure(1)
-plt.subplot(223)
-plt.autoscale(True)
-t0 = time.time()
-plt.title("Depth/Distance vs. Time (Latest data: %f)" % t0 )
-plt.ylabel("Depth/Distance (cm)")
-plt.xlabel("Time (s)")
-
-depthValue = []
-depthTime = []
-
-for ii in dbCol.find({"atype":"SONAR"}):
-    depthValue.append(ii["param"])
-    depthTime.append( ii["ts"] - t0)
-
-plt.plot(depthTime, depthValue, "mo")
-
-# Convert the lists to Numpy ndarray for later use
-depthValue = np.array( depthValue )
-depthTime = np.array( depthTime )
+if False:
+    plt.figure(15)
+    plt.autoscale(True)
+    t0 = time.time()
+    plt.title("Depth/Distance vs. Time (Latest data: %f)" % t0 )
+    plt.ylabel("Depth/Distance (cm)")
+    plt.xlabel("Time (s)")
+    
+    depthValue = []
+    depthTime = []
+    
+    for ii in dbCol.find({"atype":"SONAR"}):
+        depthValue.append(ii["param"])
+        depthTime.append( ii["ts"] - t0)
+    
+    plt.plot(depthTime, depthValue, "mo")
+    
+    # Convert the lists to Numpy ndarray for later use
+    depthValue = np.array( depthValue )
+    depthTime = np.array( depthTime )
 
 
 # ======================TEMPERATURE======================
@@ -285,7 +302,7 @@ plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
 
 # Show  the gps data within a certain time range
 plt.figure(15) ; plt.clf()
-cut = ( ( gpsTime >= tStartRun004 ) & (gpsTime <= tStopRun004) ) # Make a cut
+cut = ( ( gpsTime >= tStartRun ) & (gpsTime <= tStopRun) ) # Make a cut
 plt.plot(  [ datetime.datetime.fromtimestamp(x) for x in (gpsTime[ cut ]) ]  , gpsLat[  cut ]  , 'r.' )
 plt.gcf().axes[0].xaxis.set_major_formatter(formatter)
 
